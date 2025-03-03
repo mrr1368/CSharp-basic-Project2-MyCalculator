c5aa7ef6fabf5590fdab4be25638130ff5881721
MR. Rajabali <mrrajabali@gmail.com>
2025-03-04 02:45:49 +0330

    initial commit

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 namespace Calculator
 {
     public partial class Form1 : Form
@@ -11,12 +13,21 @@ namespace Calculator
 
         private void Number_Click(object sender, EventArgs e)
         {
-
+            if (sender is Button btn)
+            {
+                txtCalculator.Text += btn.Text; 
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            foreach (Button btn in this.Controls.OfType<Button>())
+            {
+                if (char.IsDigit(btn.Text[0]))
+                {
+                    btn.Click += Number_Click;
+                }
+            }
         }
     }
 }
